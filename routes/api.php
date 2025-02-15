0a647c2f4b8429c2c50c989be0c173f7f5f80f2b
ardianita <ardianitaf@student.uns.ac.id>
2025-02-16 00:26:41 +0700

    update routing

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\JadwalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('jadwal')->controller(JadwalController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/{jadwal}', 'show');
+    Route::post('/store', 'store');
+    Route::put('/{jadwal}/update', 'update');
+    Route::destroy('/{jadwal}/delete', 'destroy');
 });
