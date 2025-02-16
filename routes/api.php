727ac80e0dd4db35d8a9dc9d1a962556169a46ae
ardianita <ardianitaf@student.uns.ac.id>
2025-02-16 22:16:30 +0700

    update routing

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\JadwalController;
+use App\Http\Controllers\PemesananController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,11 +20,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::prefix('jadwal')->controller(JadwalController::class)->group(function () {
-    Route::get('/', 'index');
-    Route::get('/{jadwal}', 'show');
-    Route::post('/store', 'store');
-    Route::put('/{jadwal}/update', 'update');
-    Route::destroy('/{jadwal}/delete', 'destroy');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    // Route::get('/user', [AuthController::class, 'userProfile']);
+    Route::prefix('jadwal')->controller(JadwalController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('/{jadwal}', 'show');
+        Route::post('/store', 'store');
+        Route::put('/{jadwal}/update', 'update');
+        Route::delete('/{jadwal}/delete', 'destroy');
+    });
+    Route::prefix('pemesanan')->controller(PemesananController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::get('/{pemesanan}', 'show');
+        Route::post('/store', 'store');
+        Route::put('/{pemesanan}/update', 'update');
+        Route::delete('/{pemesanan}/delete', 'destroy');
+    });
 });
